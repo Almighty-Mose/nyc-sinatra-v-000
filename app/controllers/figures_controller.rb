0d00131e5d38cd04609d8eea9cadc805232fdd6b
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-23 18:56:16 +0000

    automatically backed up by learn

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class FiguresController < ApplicationController
+  get '/figures' do
+    @figure = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    erb :'figures/:id'
+  end
+
+end
